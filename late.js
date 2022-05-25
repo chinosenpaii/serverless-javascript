53dbbe95a32338b1825bff0899af66f81cdb0eab
Israel Yarce <israelhuerta1227@gmail.com>
2022-05-24 22:37:55 -0500

    fixed issue

--- late.js
+++ late.js
@@ -1,10 +1,12 @@
 // start coding your function here!
-function runnning_late(date) {
-    if (date.getHours() == 22 || date.getHours() > 22) {
+function running_late(date) {
+    if(date > "December 17, 1995 10:00:00 PM" || date == "December 17, 1995 10:00:00 PM") {
         return "It is late!";
     } else {
         return "It is still early!";
     }
-}
+};
 
+//console.log(running_late('December 17, 1995 08:00:00 AM'));
+//console.log(running_late('December 17, 1995 11:30:00 PM'));
 exports.running_late = running_late;
