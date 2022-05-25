ebd9326211b7f6cd8510a4a6dc9f5b8d653ae4ec
Israel Yarce <israelhuerta1227@gmail.com>
2022-05-24 21:42:44 -0500

    created running late function

--- late.js
+++ late.js
@@ -1,1 +1,10 @@
 // start coding your function here!
+function runnning_late(date) {
+    if (date.getHours() == 22 || date.getHours() > 22) {
+        return "It is late!";
+    } else {
+        return "It is still early!";
+    }
+}
+
+exports.running_late = running_late;
