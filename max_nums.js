7a8319d51c0b0007c9a7730b8495088691bbe0fa
Israel Yarce <israelhuerta1227@gmail.com>
2022-05-25 15:31:38 -0500

    created function to find max number in array

--- max_nums.js
+++ max_nums.js
@@ -1,1 +1,14 @@
 // start coding your function here!
+function find_max(arr) {
+    var max = 0;
+    for(var i = 0; i < arr.length; i++) {
+        if(arr[i] > max) {
+            max = arr[i];
+        }
+    }
+    return max;
+}
+//var array = [1, 78, 12, 2 , 5, 10];
+//var array2 = [1, 2, 3, 3, -1];
+//console.log(find_max(array2));
+exports.find_max = find_max;
